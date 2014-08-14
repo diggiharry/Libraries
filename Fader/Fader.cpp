2f1e040738f2df987a7a715badd769a5ebcb1cb0
diggiharry <s-l-u-r-p@gmx.de>
2014-08-14 21:57:56 +0200

    Fader now working properly

--- Fader/Fader.cpp
+++ Fader/Fader.cpp
@@ -5,134 +5,106 @@ ShiftBar sb(4,LED_values);
 
 Fader::Fader() {
     state = IDLE;
+    prev_millis = 0;
+    dur = 0;
+    start = 0;
+    target_state = IDLE;    
 }
 
 void Fader::init() {
     sb.init();
-    for (int h = 0;h<4;h++) {
-          sb.set(h,values[h*3],values[h*3+1],values[h*3+2]);  
-    }
     sb.update();
 }
 
 void Fader::update(unsigned long ms) {
     switch(state)
          {
-          case	IDLE:
-                //...
-                break;
-          case  FADE_TO_RAINBOW:
-                fade_to_rainbow(ms-start);
-                break;
-          case  RAINBOW:
-                rainbow(ms-start);
-                break;
-           case FADE_TO_COLOR:
-                fade_to_color(ms-start);
-                break;         
+            case    IDLE:
+                    //...
+                    break;
+            case    RAINBOW:
+                    rainbow();
+                    break;
+            case    FADE_TO_COLOR:
+                    fade_to_color();
+                    break;         
         }
     
     for (int h = 0;h<4;h++) {
-          sb.set(h,round(values[h*3]),round(values[h*3+1]),round(values[h*3+2]));  
+          sb.set(h,floor(values[h*3]),floor(values[h*3+1]),floor(values[h*3+2]));  
     }
+    
     sb.update();
 }
 
-void Fader::fade_to_color(unsigned long ms) {
-    float factor = 0.05; // increase per millisecond
-    int err = 0;
+void Fader::fade_to_color() {
+    unsigned long curr_millis = millis();
+    float diff_millis = curr_millis - prev_millis;
+    prev_millis = curr_millis;
+
     for (int h = 0;h<12;h++) {
-        int buf = (values[h]-target_values[h]);
-        if (buf < 0) values[h] = values[h] + factor*ms;
-        if (buf > 0) values[h] = values[h] - factor*ms;
-        err += abs(buf); 
-    }    
-    if (err < 40) {
-        state = IDLE;
+        values[h] = values[h] + slopes[h] * diff_millis;
+     }   
+    if (curr_millis >= (start + dur ) ) {
+        state = target_state;
         for (int h = 0;h<12;h++) {
             values[h] = target_values[h];
         }
     }   
-    start = millis();
 }
 
-void Fader::start_fade_to_color(int colors[12]) {   
-    state = FADE_TO_COLOR;
+void Fader::start_fade_to_color(int colors[12], unsigned long duration) {   
     for (int h = 0;h<12;h++) {
-        target_values[h] = colors[h];
+        target_values[h] =  (float) colors[h];
+        slopes[h] = ( target_values[h] - values[h] ) / duration  ;
     }
+    dur = duration;
+    target_state = IDLE;
+    state = FADE_TO_COLOR;
     start = millis();
+    prev_millis = start; 
 }
 
 int Fader::triangle_function(unsigned long ms,unsigned int period, int phase) {
-    return round( 1023 * 2 * abs(round( ( (float) ms+phase)/period)-( ( (float) ms+phase)/ period)) );    
-}
-
-void Fader::fade_to_rainbow(unsigned long ms) {
-    float factor = 0.05; // increase per millisecond
-    int err = 0;
-    for (int h = 0;h<12;h++) {
-        int buf = (values[h]-target_values[h]);
-        if (buf < 0) values[h] = values[h] + factor*ms;
-        if (buf > 0) values[h] = values[h] - factor*ms;
-        err += abs(buf); 
-    }    
-    if (err < 40) {
-        state = RAINBOW;
-    }
-    start = millis();
+    return 1023 * 2 * abs(round( ( (float) ms+phase)/period)-( ( (float) ms+phase)/ period)) ;    
 }
 
 void Fader::start_rainbow() {
-    state = FADE_TO_RAINBOW;
     int red = triangle_function(0,period,0);
     int green = triangle_function(0,period,phase_green);
     int blue = triangle_function(0,period,phase_blue);
-    target_values[0] = red;
-    target_values[1] = green;
-    target_values[2] = 0;
-    
-    target_values[3] = 0;
-    target_values[4] = green;
-    target_values[5] = blue;
-    
-    target_values[6] = red;
-    target_values[7] = 0;
-    target_values[8] = blue;
-    
-    target_values[9] = red;
-    target_values[10] = green;
-    target_values[11] = blue;
-    start = millis();
+    int start_values[12] = { red,green,0, 0,green,blue, red,0,blue, red,green,blue }; 
+    start_fade_to_color(start_values,3000);
+    target_state = RAINBOW;
+    //start = millis();
 }
 
-void Fader::rainbow(unsigned long ms) {
-    //unsigned long ms= millis();
-
+void Fader::rainbow() {
+    unsigned long diff_millis = millis() - start - 3000;
+    Serial.println(diff_millis);
     /*red = floor( 712+311 * (double)  cos(2*PI/period*ms) );
     green = floor( 712+311 * (double) cos(2*PI/period*(phase_green-ms)));
     blue = floor( 712+311 * (double) cos(2*PI/period*(phase_blue-ms)));*/
-    int red = triangle_function(ms,period,0);
-    int green = triangle_function(ms,period,phase_green);
-    int blue = triangle_function(ms,period,phase_blue);
-    
+    float red = triangle_function(diff_millis,period,0);
+    float green = triangle_function(diff_millis,period,phase_green);
+    float blue = triangle_function(diff_millis,period,phase_blue);
+
     values[0] = red;
     values[1] = green;
     values[2] = 0;
-    
+
     values[3] = 0;
     values[4] = green;
     values[5] = blue;
-    
+
     values[6] = red;
     values[7] = 0;
     values[8] = blue;
-    
+
     values[9] = red;
     values[10] = green;
     values[11] = blue;
 }
-
 
 /**
 * 
@@ -154,7 +126,7 @@ void Fader::rainbow(unsigned long ms) {
 * @param Number b The blue color value
 * @return Array The HSV representation
 */
-void Fader::rgbToHsv(byte r, byte g, byte b, double hsv[]) {
+/*void Fader::rgbToHsv(byte r, byte g, byte b, double hsv[]) {
     double rd = (double) r/255;
     double gd = (double) g/255;
     double bd = (double) b/255;
@@ -177,7 +149,7 @@ void Fader::rgbToHsv(byte r, byte g, byte b, double hsv[]) {
     hsv[0] = h;
     hsv[1] = s;
     hsv[2] = v;
-}
+}*/
 
 
 /**
@@ -199,7 +171,7 @@ void Fader::rgbToHsv(byte r, byte g, byte b, double hsv[]) {
 * @param Number v The value
 * @return Array The RGB representation
 */
-void Fader::hsvToRgb(double h, double s, double v, byte rgb[]) {
+/*void Fader::hsvToRgb(double h, double s, double v, byte rgb[]) {
     double r, g, b;
     int i = int(h * 6);
     double f = h * 6 - i;
@@ -217,4 +189,4 @@ void Fader::hsvToRgb(double h, double s, double v, byte rgb[]) {
     rgb[0] = r * 255;
     rgb[1] = g * 255;
     rgb[2] = b * 255;
-}
+}*/
--- Fader/Fader.h
+++ Fader/Fader.h
@@ -13,7 +13,6 @@
 
 //define states
 #define IDLE 0
-#define FADE_TO_RAINBOW 1
 #define RAINBOW 2
 #define FADE_TO_COLOR 3
 
@@ -23,24 +22,27 @@ class Fader {
         void update(unsigned long ms);
         void init();
         void start_rainbow();        
-        void start_fade_to_color(int colors[12]);
+        void start_fade_to_color(int colors[12],unsigned long duration);
         
     private: 
         int state;   
         float values[12] = {0,0,0, 0,0,0, 0,0,0, 0,0,0};
         float target_values[12] = {0,0,0, 0,0,0, 0,0,0, 0,0,0};   
+        float slopes[12] = {0,0,0, 0,0,0, 0,0,0, 0,0,0};   
+        unsigned long prev_millis;
+        unsigned long dur;
+        unsigned long start;
+        int target_state;
         
         int triangle_function(unsigned long ms,unsigned int period, int phase);
-        void fade_to_rainbow(unsigned long ms);
-        void fade_to_color(unsigned long ms);
-    
+        void fade_to_color();
+
         void rgbToHsv(byte r, byte g, byte b, double hsv[]);
         void hsvToRgb(double h, double s, double v, byte rgb[]);
-        int phase_green = 1500;
-        int phase_blue = 3000;
-        int period = 5000;
-        unsigned long start;
-        void rainbow(unsigned long ms);
+        int phase_green = 500;
+        int phase_blue = 1000;
+        int period = 2000;
+        void rainbow();
 
 };
 
